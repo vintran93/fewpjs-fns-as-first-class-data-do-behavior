c6a5371f6e534248fadd67d93faa0c911bccb1b2
Vincent Tran <vincentuong@yahoo.com>
2022-01-13 15:46:31 -0600

    commit tree becc1d853fefb885ab5e5f7e890242353bf19645

--- index.js
+++ index.js
@@ -9,5 +9,16 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
+
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  document.getElementById("greeting").textContent = msg;
+}
+
+/* Write your implementation of greet() */
+function greet(String) {
+  const hour = parseInt(String);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
